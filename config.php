afd7b43016ada2a3001efa7ac64abd4b8c03853a
Bashir Uddin <uddinbashir1999@gmail.com>
2023-05-13 19:52:48 +0500

    fix: create config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  # configurations for database
+  "database" => [
+    "host" => "127.0.0.1", # host
+    "dbname" => "databasename", # database name
+    "user" => "phpmyadmin", # username
+    "password" => "root" #password
+
+  ],
+  # configurations for JWT
+  'jwt_secret' => 'thisisalongjwtsecretandthisisalongjwtsecret'
+];
