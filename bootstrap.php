ce4bbf9de2fcb5c546e47bee2ddeda03fef3ae22
Bashir Uddin <uddinbashir1999@gmail.com>
2023-05-13 20:01:10 +0500

    fix: add entry point  + routes + bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\{App, JwtAuth, Request, Router};
+
+App::bind('config', require "config.php");
+JwtAuth::setSecretKey(App::get('config')['jwt_secret']);
+
+
+# routes configuration
+Router::requireFile('routes.php')->show(
+  Request::uri(),
+  Request::method()
+);
---
+++ routes.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$router->post('login', 'LoginController@login');
+$router->post('signup', 'LoginController@signup');
