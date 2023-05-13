eebe312a645d7da199c89550f0db7df6579b2969
Bashir Uddin <uddinbashir1999@gmail.com>
2023-05-13 20:29:41 +0500

    chore: fix indentation

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,8 @@
 use App\{App, JwtAuth, Request, Router};
 
 App::bind('config', require "config.php");
-JwtAuth::setSecretKey(App::get('config')['jwt_secret']);
 
+JwtAuth::setSecretKey(App::get('config')['jwt_secret']);
 
 # routes configuration
 Router::requireFile('routes.php')->show(
